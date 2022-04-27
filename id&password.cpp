89d2385f7281062089939fccc2900323fad61bbb
youssef mohmed <youssefmohmedyahoo.com9@gmail.com>
2022-04-27 15:11:31 +0200

    (12)login app while enter the password it turn into *

---
+++ id&password.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+#include <iostream>
+
+#include <cmath>
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
+#include<bits/stdc++.h>
+
+#include<conio.h>
+
+
+using namespace std;
+
+int shifts ,len;
+
+struct user_information{
+
+ int id;
+
+ char password[20];
+
+
+};
+
+
+int main()
+{
+    user_information e1 ;
+
+    int i;
+
+    i=0;
+
+    cout<<"please enter your id : ";
+
+    cin>>e1.id;
+
+
+
+    cout<<"please enter your password : ";
+
+    char ch;
+
+    for(i=0;(ch=getch())!='\r';){
+
+        if(ch!=8){
+
+            e1.password[i]=ch;
+
+            cout<<"*";
+
+            i++;
+        }
+        else{
+             i--;
+
+             if(i<0){
+                i++;
+             }
+             else{
+                cout<<"\b \b";
+             }
+
+
+        }
+
+    }
+
+    e1.password[i]='\0';
+
+    cout<<endl<<e1.password;
+
+
+
+
+    return 0;
+}
