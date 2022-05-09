d22428748f71778740cdb00cbbe95be30ac3e1ff
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2022-05-09 16:36:09 +0200

    the changes we do in the FCAI

--- User.h
+++ User.h
@@ -1,36 +1,58 @@
-#include<regex>
+#pragma once
 #include<iostream>
+#include<fstream>
+#include<sstream> 
 #include<string>
 #include<conio.h>
-#include<fstream>
-
+#include<regex>
+#include<vector>
 
 using namespace std;
 
-class User{
-    private:
-        //declare attrs and private methods
-        string username;
-        string email;
-        string password;
-        string phoneNumber;
-        bool isValidEmail(string e);
-        bool isValidUsername(string u);
-        bool isValidPhoneNumber(string p);
-        bool isValidPassword(string p);
-        void encryptPassword(string p);
-        void decryptPassword(string p);
-        void saveChanges(string changes);
-    public:
-        //declare getter
-        string getUsername();
-        string getEmail();
-        string getPhoneNumber();
-        //declare setter
-        void setUsername();
-        void setEmail();
-        void setPhoneNumber();
-        void setPassword();
+class User {
+
+public:
+    //declare attrs
+    string username;
+    string email;
+    string password;
+    string phoneNumber;
+    vector<string> usernames;
+    vector<string> phoneNumbers;
+    vector<string> emails;
+    vector<string> passwords;
+
+    int trail;
+
+    //declare validation
+    bool isValidEmail(string e);
+    bool isValidUsername(string u);
+    bool isValidPhoneNumber(string p);
+    bool isValidPassword(string p);
+    bool isFoundedUsername(string username);
+    bool isFoundedEmail(string email);
+
+    ////declare getters
+    //string getUsername();
+    string getUsernamePasswords(string username);
+    //string getEmail();
+    //string getPhoneNumber();
+    string encryptPassword(string p);
+
+    //declare setters
+    void saveChanges(string changes,string type);
+    void setUsername();
+    void setEmail();
+    void setPhoneNumber();
+    void setPassword();
+
+    //declare operations
+    void registration();
+    void login();
+    void changePassword();
+
+    User();
+    ~User();
 
 
 };
--- main.cpp
+++ main.cpp
@@ -1,39 +1,45 @@
 #include<iostream>
 #include "User.h"
-
 using namespace std;
 
-int8_t userChoise;
+int userChoise;
 
 
-void enterValidChoise(string promptMsg,int min,int max);
-void registering();
+void enterValidChoise(string promptMsg, int min, int max);
 
-int main(){
-    
-    enterValidChoise("1- Register \n2- Login \n3- Change Password \n4- Exit",1,4);
+int main() {
 
-}
-
-
-void enterValidChoise(string promptMsg,int min,int max){
-    while (true){
-        cout<<promptMsg<<endl;
-        cout<<"--> ";
-        cin>>userChoise;
-        if (userChoise<= max && userChoise >= min){
-            break;
-        }
-        else
-            cout<<"Please, Enter num between "<<min<<" and "<<max<<"!!..\n";
+    enterValidChoise("1- Register \n2- Login \n3- Change Password \n4- Exit", 1, 4);
+    switch (userChoise)
+    {
+    case 1:
+    {
+        User user;
+        user.registration();
+        break;
+    }
+    case 2:
+    {
+        User user;
+        user.login();
+        break;
+    }
+    default:
+        break;
     }
 }
 
-void registering(){
-    User user;
-    user.setUsername();
-    user.setEmail();
-    user.setPhoneNumber();
-    user.setPassword();
+
+void enterValidChoise(string promptMsg, int min, int max) {
+    while (true) {
+        cout << promptMsg << endl;
+        cout << "--> ";
+        cin >> userChoise;
+        if (userChoise <= max && userChoise >= min) {
+            break;
+        }
+        else
+            cout << "Please, Enter num between " << min << " and " << max << "!!..\n";
+    }
 
 }
